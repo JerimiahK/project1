8574d1cd5de2e4c9c378e335ce6831f4308c7385
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-19 10:24:17 -0700

    phone scaling

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -9,7 +9,7 @@ window.onload = function () {
   });
 
   window.addEventListener("scroll", () => {
-    const scrollVert = window.scrollY * 0.00059;
+    const scrollVert = window.scrollY * 0.000395;
     console.log(scrollVert);
     verticleProg[0].style.transform = `scaleY(${scrollVert}`;
   });
