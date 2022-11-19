ce18c1f7eae110d9fcf1c4c3b79d11d3f84c274c
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-19 10:30:49 -0700

    scaling

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -9,7 +9,7 @@ window.onload = function () {
   });
 
   window.addEventListener("scroll", () => {
-    const scrollVert = window.scrollY * 0.000399;
+    const scrollVert = window.scrollY * 0.00045;
     console.log(scrollVert);
     verticleProg[0].style.transform = `scaleY(${scrollVert}`;
   });
