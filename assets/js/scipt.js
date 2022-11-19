362c80e43430b372cc989d378b4b50706db03346
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-19 09:23:36 -0700

    made a verticle scroll bar for small devices

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -3,13 +3,14 @@ window.onload = function () {
   const verticleProg = $("#scrollVert");
 
   window.addEventListener("scroll", () => {
-    const scrolled = window.scrollX * 0.000208335;
+    const scrolled = window.scrollX * 0.00023;
     horizontalProg[0].style.transform = `scaleX(${scrolled}`;
+    console.log(scrolled);
   });
 
   window.addEventListener("scroll", () => {
-    const scrollVert = window.scrollY * 0.00108335;
-    console.log(scrollVert)
+    const scrollVert = window.scrollY * 0.00072;
+    console.log(scrollVert);
     verticleProg[0].style.transform = `scaleY(${scrollVert}`;
   });
 };
