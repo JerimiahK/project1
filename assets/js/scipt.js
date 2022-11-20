1e8f257752994c6e4b5a736f2567396f2d567fc4
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-19 23:47:35 -0700

    started on adding header text

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -3,8 +3,11 @@ window.onload = function () {
   let vertProgress = $("#scrollVertProgress");
 
   window.onscroll = function () {
+    // page scroll x axis
     let percentScrolled = window.scrollX / window.innerWidth / 3;
     progress[0].style.transform = `scaleX(${percentScrolled}`;
+    
+    // page scroll y axis
     let vertPercentScrolled = window.scrollY / window.innerHeight / 3;
     vertProgress[0].style.transform = `scaleY(${vertPercentScrolled})`;
   };
