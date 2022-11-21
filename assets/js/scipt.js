ef8817f1bd33036d177391d76dd9e153414e356f
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-20 22:51:22 -0700

    adjusted about on mobile view

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -5,17 +5,13 @@ window.onload = function () {
   window.onscroll = function () {
     // page scroll x axis
     let percentScrolled = window.scrollX / window.innerWidth / 2.5;
-    console.log(percentScrolled);
     progress[0].style.transform = `scaleX(${percentScrolled}`;
 
     // page scroll y axis
-    let vertPercentScrolled = window.scrollY / window.innerHeight / 2.5;
+    let vertPercentScrolled = window.scrollY / window.innerHeight / 3;
     vertProgress[0].style.transform = `scaleY(${vertPercentScrolled})`;
-  };
 
-  // window.onscroll = function () {
-  //   let vertPercentScrolled = window.scrollY / window.innerHeight / 3;
-  //   vertProgress[0].style.transform = `scaleY(${vertPercentScrolled})`;
-  //   console.log(vertProgress);
-  // };
+    let scrollArrow = $("#scrollArrow");
+    scrollArrow[0].style.display = "none";
+  };
 };
