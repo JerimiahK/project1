81fc1f975be2c70db5738900d1390ab393fb0c39
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-20 18:47:00 -0700

    added an info section, and an about me within that

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -4,11 +4,12 @@ window.onload = function () {
 
   window.onscroll = function () {
     // page scroll x axis
-    let percentScrolled = window.scrollX / window.innerWidth / 3;
+    let percentScrolled = window.scrollX / window.innerWidth / 2.5;
+    console.log(percentScrolled);
     progress[0].style.transform = `scaleX(${percentScrolled}`;
-    
+
     // page scroll y axis
-    let vertPercentScrolled = window.scrollY / window.innerHeight / 3;
+    let vertPercentScrolled = window.scrollY / window.innerHeight / 2.5;
     vertProgress[0].style.transform = `scaleY(${vertPercentScrolled})`;
   };
 
